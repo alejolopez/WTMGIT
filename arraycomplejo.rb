57111fb87b837e4d2416525c22ac24e9fccd0d81
Alejandro Lopez <AlejoLopez@MacBook-Air-de-Alejandro.local>
2014-08-06 19:27:07 -0500

    ajustearray

--- arraycomplejo.rb
+++ arraycomplejo.rb
@@ -1,6 +1,6 @@
 puts "array ahora que multiplique por 3 cada campo del array, ingrese hasta cuanto" 
 n = gets.chomp.to_i 
-lista = (0..n).to_a # to a mete los elementos en el array
+lista = (0..n).to_a # to a mete los elementos en el array para despues organizarlos
 puts lista
 lista.each do |i| 
 	puts "multiplicado: #{i*3}"
