f2b5017272620dff42d17df67a9f69fe60467fd9
Laura Maria <lrmckn@gmail.com>
2014-08-06 19:42:36 -0500

    cambio en tu archivo

--- arraycomplejo.rb
+++ arraycomplejo.rb
@@ -1,3 +1,4 @@
+#HOLA ALEJO!! :)
 puts "array ahora que multiplique por 3 cada campo del array, ingrese hasta cuanto" 
 n = gets.chomp.to_i 
 lista = (0..n).to_a # to a mete los elementos en el array para despues organizarlos
